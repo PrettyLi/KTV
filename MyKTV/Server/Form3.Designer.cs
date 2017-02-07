be6df01bbdb85da41bea9c1f0e814a11e3e43089
冯亮 <1114740678@qq.com>
2017-02-07 16:46:52 +0800

    加了个窗体

--- MyKTV/Server/Form3.Designer.cs
+++ MyKTV/Server/Form3.Designer.cs
@@ -33,21 +33,24 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(228, 133);
+            this.button1.Location = new System.Drawing.Point(304, 166);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 29);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(481, 392);
+            this.ClientSize = new System.Drawing.Size(641, 490);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form3";
             this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
             this.ResumeLayout(false);
 
         }
---
+++ MyKTV/Server/Form4.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Server
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.awefToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.awefToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.awefToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.afdvcToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.awefToolStripMenuItem,
+            this.awefToolStripMenuItem1,
+            this.awefToolStripMenuItem2,
+            this.afdvcToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(778, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // awefToolStripMenuItem
+            // 
+            this.awefToolStripMenuItem.Name = "awefToolStripMenuItem";
+            this.awefToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.awefToolStripMenuItem.Text = "awef";
+            // 
+            // awefToolStripMenuItem1
+            // 
+            this.awefToolStripMenuItem1.Name = "awefToolStripMenuItem1";
+            this.awefToolStripMenuItem1.Size = new System.Drawing.Size(55, 24);
+            this.awefToolStripMenuItem1.Text = "awef";
+            // 
+            // awefToolStripMenuItem2
+            // 
+            this.awefToolStripMenuItem2.Name = "awefToolStripMenuItem2";
+            this.awefToolStripMenuItem2.Size = new System.Drawing.Size(55, 24);
+            this.awefToolStripMenuItem2.Text = "awef";
+            // 
+            // afdvcToolStripMenuItem
+            // 
+            this.afdvcToolStripMenuItem.Name = "afdvcToolStripMenuItem";
+            this.afdvcToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.afdvcToolStripMenuItem.Text = "afdvc";
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 567);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem awefToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem awefToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem awefToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem afdvcToolStripMenuItem;
+    }
+}
